da866ee79a3a89ef49af0afe70c7fa546a6d34c2
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-06 16:31:33 +0900

    コードでルートVCを設定

--- RxSwiftREADME/SceneDelegate.swift
+++ RxSwiftREADME/SceneDelegate.swift
@@ -7,16 +7,18 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+//        let vc = ViewController.instantiate()
+        let vc = WithoutRxCoccoaViewController.instantiate()
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
