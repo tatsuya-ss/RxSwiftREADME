b01f091e6853c402b2723951fbcbe7302b8279b8
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-07 19:23:52 +0900

    Tableviewの初期設定

--- RxSwiftREADME/WithoutRxCoccoa/Table/Cell/TableViewCell.swift
+++ RxSwiftREADME/WithoutRxCoccoa/Table/Cell/TableViewCell.swift
@@ -7,17 +7,27 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
-
+final class TableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var checkImage: UIImageView!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+    func configure(title: String, isHidden: Bool) {
+        titleLabel.text = title
+        checkImage.isHidden = isHidden
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
     
 }
--- RxSwiftREADME/WithoutRxCoccoa/Table/TableViewController.swift
+++ RxSwiftREADME/WithoutRxCoccoa/Table/TableViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+final class TableViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init() {
+        super.init(nibName: String(describing: TableViewController.self),
+                   bundle: nil)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+// MARK: - setup
+extension TableViewController {
+    
+    private func setupTableView() {
+        tableView.register(TableViewCell.nib,
+                           forCellReuseIdentifier: TableViewCell.identifier)
+    }
+    
 }
